67ee8056232de4e0dea2eb1ec3d62d99636246d4
EC2 Default User <ec2-user@ip-172-31-33-94.us-east-2.compute.internal>
2020-09-11 18:55:24 +0000

    Current Server Version

--- ParticipantWebPage/index.js
+++ ParticipantWebPage/index.js
@@ -2,18 +2,25 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+var connectCounter = 0;
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
     });
 
 io.on('connection', (socket) => {
-	console.log('a user connected');
+        console.log('a user connected');
+        connectCounter++;
+        // Send number of users
 	socket.on('disconnect', () => {
-		console.log('user disconnected');
+	    console.log('user disconnected');
+	    connectCounter--;
+	    // Send number of uers
 	    });
-	socket.on('chat message', (msg) => {
-		console.log('message: ' + msg);
-		io.emit('chat message', msg);
+        socket.on('chat message', (msg) => {
+	    console.log('message: ' + connectCounter); // Replace with <user> clapped
+	    io.emit('chat message', connectCounter); // Replace with <user> clapped
+	    console.log('total num clients: ' + connectCounter); // Remove
 	    });
     });
 
