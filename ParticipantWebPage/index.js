a2339bcb345da2006328b9d50850e27bcb006225
thely <bardbarienne@gmail.com>
2020-10-02 15:05:13 -0400

    many JS fixes to make back/frontend work

--- ParticipantWebPage/index.js
+++ ParticipantWebPage/index.js
@@ -1,3 +1,5 @@
+const { connect } = require('http2');
+
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
@@ -5,26 +7,39 @@ var io = require('socket.io')(http);
 var connectCounter = 0;
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
-    });
+  // http://localhost:4000
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/max', (req, res) => {
+  // http://localhost:4000/max
+  res.sendFile(__dirname + '/indexmax.html');
+})
 
 io.on('connection', (socket) => {
-        console.log('a user connected');
-        connectCounter++;
-        // Send number of users
+  console.log('a user connected');
+  connectCounter++;
+  console.log("inside connection: " + connectCounter);
+  // Send number of users
+
+  io.sockets.emit('onConnect', connectCounter);
+
 	socket.on('disconnect', () => {
-	    console.log('user disconnected');
-	    connectCounter--;
-	    // Send number of uers
-	    });
-        socket.on('chat message', (msg) => {
-	    console.log('message: ' + connectCounter); // Replace with <user> clapped
-	    io.emit('chat message', connectCounter); // Replace with <user> clapped
-	    console.log('total num clients: ' + connectCounter); // Remove
-	    });
-    });
+    console.log('user disconnected');
+    connectCounter--;
+    io.sockets.emit('onConnect', connectCounter);
+    console.log("inside disconnect: " + connectCounter);
+    // Send number of uers
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + connectCounter); // Replace with <user> clapped
+    io.emit('chat message', connectCounter); // Replace with <user> clapped
+    console.log('total num clients: ' + connectCounter); // Remove
+  });
+});
 
 
-http.listen(3000, () => {
-	console.log('listening on *:3000');
+http.listen(4000, () => {
+	console.log('listening on *:4000');
     });
